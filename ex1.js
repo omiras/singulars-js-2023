85ea04c79493fc510b1f056d46e0f47c280e71c6
Cynox336 <cynox336@gmail.com>
2023-05-04 13:59:30 +0200

    Update 04/05 13:59

--- ex1.js
+++ ex1.js
@@ -8,7 +8,18 @@
  * Si el esperma contiene el cromosoma X, devuelva "¡Felicitaciones! Tendrás una hija."; Si el esperma contiene el cromosoma Y, devuelva "¡Felicitaciones! Tendrás un hijo.". Cualquier otra combinación no es posible, y deberíamos devolver un mensaje de esta forma: "Lo siento, el hijo no se llevará a termino."
  */
 
-function chromosomeCheck(chromosomes) {}
+function chromosomeCheck(chromosomes) {
+    if (chromosomes === "XY") {
+      return "¡Felicitaciones! Tendrás un hijo.";
+    } else if (chromosomes === "XX") {
+      return "¡Felicitaciones! Tendrás una hija.";
+    } else {
+      return "Lo siento, el hijo no se llevará a término.";
+    }
+  }
+  /** En el codigo de arriba lo que he hecho es que confirme que la combiancion de letras XX o XY esta, si no esta dara "error" por lo 
+   * que saltara un "error" en el que dira que Lo siente pero el hijo no se llevará a término ya que no tiene las letras XX o XY
+   */
 
 console.log(chromosomeCheck("XY")); // "Felicitaciones! Tendrás un hijo."
 console.log(chromosomeCheck("XX")); // "Felicitaciones! Tendrás una hija."
