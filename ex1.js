f000d87376a17414fcd34bee4ffbebaeed36ec3c
Dina <dina.yamanid@gmail.com>
2023-05-04 13:01:39 +0200

    dom done

--- ex1.js
+++ ex1.js
@@ -8,7 +8,11 @@
  * Si el esperma contiene el cromosoma X, devuelva "¡Felicitaciones! Tendrás una hija."; Si el esperma contiene el cromosoma Y, devuelva "¡Felicitaciones! Tendrás un hijo.". Cualquier otra combinación no es posible, y deberíamos devolver un mensaje de esta forma: "Lo siento, el hijo no se llevará a termino."
  */
 
-function chromosomeCheck(chromosomes) {}
+function chromosomeCheck(chromosomes) {
+    if(chromosomes === "XY"){
+        return "Felicitaciones! Tendrás un hijo."
+    }
+}
 
 console.log(chromosomeCheck("XY")); // "Felicitaciones! Tendrás un hijo."
 console.log(chromosomeCheck("XX")); // "Felicitaciones! Tendrás una hija."
